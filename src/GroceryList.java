23dc69db0572c8e44b3bee1f51e5d90e74847d45
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-02-18 02:22:40 +0300

    the for loop

---
+++ src/GroceryList.java
@@ -0,0 +1,9 @@
+public class GroceryList{
+    public static void main(String[] args) {
+        String [] groceryItems = {"bananas", "mangoes", "apples", "pens", "books", "cabbages"};
+
+        for (Integer index = 0 ; index < groceryItems.length ; index++){
+            System.out.println(groceryItems[index]);
+        }
+    }
+}
